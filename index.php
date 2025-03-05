0fdac17b423a7e898e3dcb64211cacb4b08f4ec1
thanosdrome <ranjanshudhanshu104@gmail.com>
2025-03-05 18:23:00 +0530

    blog page

--- index.php
+++ index.php
@@ -10,6 +10,27 @@
       </div>
     </div>
 
+  <div class="container container--narrow page-section">
+  <?php 
+    while(have_posts()){
+      the_post();?>
+      <div class="post-item">
+        <h2 class="headline healing--medium headline--post-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+        <div class="metabox">         
+          <p>Post By <?php the_author_posts_link( );?>  on <?php the_time( 'n.j.y' );?> in <?php echo get_the_category_list(', '); ?><p>
+        </div>
+        <div>
+          <?php  the_excerpt( )?>
+          <p><a class="btn btn--blue" href="<?php the_permalink();?>">Continue Reading &raquo;</a></p>
+        </div>
+      </div> 
+   
+  <?php
+  }
+echo paginate_links ();
+
+?> 
+</div>
 
   <?php get_footer();
 
