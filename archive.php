22ee810157ccc411befa55fca6bd74db8e1fc7cb
thanosdrome <ranjanshudhanshu104@gmail.com>
2025-03-05 19:00:52 +0530

    Added Features

--- archive.php
+++ archive.php
@@ -1,12 +1,18 @@
 <?php get_header(); ?>
 
-  <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
-      <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">Welcome!</h1>
-        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg')?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php if(is_category()){
+            single_cat_title();
+        }
+        if (is_author()){
+            echo 'Posts By '; the_author();
+        }
+        ?></h1>
+        <div class="page-banner__intro">
+          <?php the_archive_description();?>
+        </div>
       </div>
     </div>
 
